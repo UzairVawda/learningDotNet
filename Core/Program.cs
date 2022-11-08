c6f57ba01352633d5890553a266a275b8f33601b
Vawda <E40004367@ccanet.rockwellcollins.com>
2022-11-08 10:39:26 -0500

    adding more middleware to modify incoming requests

--- Core/Program.cs
+++ Core/Program.cs
@@ -1,56 +1,29 @@
 // create builder sets up the basic features for asp
 // creating servires for configuration, logging etc..
 using Core;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.Configure<FruitOptions>(option =>
+{
+    option.Name = "Watermelon";
+});
 
 // sets up the middleware component
 var app = builder.Build();
 
-// working with the reques and passing it through middlewares
-
-// // when any get request with custom = true query string
-// app.Use(async (context, next) =>
-// {
-// if (context.Request.Method == HttpMethods.Get && context.Request.Query["custom"] == "true")
-// {
-// context.Response.ContentType = "text/plain";
-// await context.Response.WriteAsync("Custom MiddleWare\n");
-//     }
-// await next();
-//     // awaire next().invoke
-// });
-
-// changing the request as it is being passed 
-// app.Use(async (context, next) => 
-// {
-// await next();
-// await context.Response.WriteAsync($"\nStatus code: {context.Response.StatusCode}");
-
-// if (context.Request.Path == "/short")
-// {
-// await context.Response.WriteAsync("REQUEST SHORT CIRCUIT");
-//     } 
-//     else
-// {
-// await next();
-//     }
-// });
-
-// terminal middleware
-
-((IApplicationBuilder)app).Map("/branch", branch =>
+app.MapGet("/fruit", async (HttpContext context, IOptions<FruitOptions> FruitOptions) => 
 {
-    branch.Run(new Middleware().Invoke);
+    FruitOptions options = FruitOptions.Value;
+    await context.Response.WriteAsync($"{options.Name}, {options.Color}");
+
 });
 
-
-app.UseMiddleware<Middleware>();
-
+app.UseMiddleware<FruitMiddleware>();
 
 // apeget middleware to get route (endpoint) `/` to return string
 app.MapGet("/", () => "Hello World!");
-
 
 // run
 app.Run();
